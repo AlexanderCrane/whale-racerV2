3f8745d992a5f2691cc70c39c043d22c67581788
Jakob Wulf-Eck <jakobweck@gmail.com>
2017-10-28 20:13:22 -0500

    Added navmesh and AI whale that paths to the first checkpoint and stops. Really janky.

---
+++ Whale Racer V2/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+
+    public Transform goal;
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        bool worked = agent.SetDestination(goal.position);
+        Debug.Log(worked);
+        Debug.Log(agent.destination);
+    }
+}
---
+++ Whale Racer V2/Assets/Scripts/PathingWaypoint.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//based on James Arndt's Unity Simple Vehicle AI tutorial - adapted from his JS sript
+//at any given time, ai whale's selected PathingWaypoint will be the waypoint it is HEADING TOWARDS
+//currently unused because I discovered the navmesh system, but will probably be used to guide navmeshagent between checkpts
+public class PathingWaypoint : MonoBehaviour
+{
+    //the first waypoint - shared by all waypoint scripts
+    public static PathingWaypoint first;
+    //pointer to the next waypoint after this one
+    public PathingWaypoint next;
+    bool isStart = false;
+    public Vector3 CalculateTarget(Vector3 position)
+    {
+        //once close to the waypoint we're heading towards, start heading towards the one after that in advance
+        if (Vector3.Distance(transform.position, position) < 6)
+        {
+            return next.transform.position;
+        }
+        //else just return the current target
+        return transform.position;
+    }
+
+    // Update is called once per frame
+    void Awake()
+    {
+        if (isStart)
+        {
+            first = this;
+        }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, .3f);
+        Gizmos.DrawCube(transform.position, new Vector3(5, 5, 5));
+
+        if (next)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, next.transform.
+            position);
+        }
+    }
+}
