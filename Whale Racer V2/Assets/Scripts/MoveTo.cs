f3de852ad4726ce715287ee69d8d2a6b41edca56
Jakob Wulf-Eck <jakobweck@gmail.com>
2017-11-01 20:18:44 -0500

    AI whale has movement animation

--- Whale Racer V2/Assets/Scripts/MoveTo.cs
+++ Whale Racer V2/Assets/Scripts/MoveTo.cs
@@ -4,14 +4,29 @@ using UnityEngine.AI;
 
 public class MoveTo : MonoBehaviour
 {
+    public Animator whaleAnimator;
+    private AnimationHashTable animations;
 
     public GameObject goal;
 
     void Start()
     {
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        whaleAnimator = gameObject.GetComponent<Animator>();
+
         Debug.Log(agent.transform.rotation);
         Debug.Log(agent.SetDestination(goal.transform.position));
+    }
+    void Update()
+    {
+        if (animations == null)
+        {
+            animations = GameManager.gmInst.GetComponent<AnimationHashTable>();
+        }
+        if (GetComponent<NavMeshAgent>().velocity.magnitude > 2f)
+        {
+            whaleAnimator.SetFloat(animations.moveFloat, GetComponent<NavMeshAgent>().velocity.magnitude / 2);
+        }
     }
 
 
