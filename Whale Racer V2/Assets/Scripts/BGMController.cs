a17dd5a595cebb9e73ef3f96c4e19fc30f88121a
Jakob Wulf-Eck <jakobweck@gmail.com>
2017-11-05 15:45:10 -0600

    Underwater sound toggles when diving

---
+++ Whale Racer V2/Assets/Scripts/BGMController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMController : MonoBehaviour {
+
+    public AudioClip aboveWaterClip;
+    public AudioClip underWaterClip;
+    public AudioSource audioPlayer;
+    public static BGMController bgmInst;
+    void Awake()
+    {
+        if (bgmInst == null)
+        {
+            bgmInst = this;
+        }
+    }
+    public void ToggleAboveWaterSound()
+    {
+        if (audioPlayer.clip != aboveWaterClip)
+        {
+            audioPlayer.clip = aboveWaterClip;
+            audioPlayer.Play();
+        }
+    }
+    public void ToggleUnderWaterSound()
+    {
+        if (audioPlayer.clip != underWaterClip)
+        {
+            audioPlayer.clip = underWaterClip;
+            audioPlayer.Play();
+        }
+    }
+}
