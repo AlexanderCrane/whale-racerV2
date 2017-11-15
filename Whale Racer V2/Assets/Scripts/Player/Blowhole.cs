18a17c3afd242e6c51a3e47644f5b499089d7e26
Jakob Wulf-Eck <jakobweck@gmail.com>
2017-11-14 19:08:03 -0600

    Initial implementation of blowhole using splash art

---
+++ Whale Racer V2/Assets/Scripts/Player/Blowhole.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blowhole : MonoBehaviour {
+    private string blowholeButton = "Blowhole";
+    public GameObject blowholeVisual;
+	void Awake()
+    {
+        if (gameObject.GetComponent<PlayerMovement>().splitscreenPlayer2)
+        {
+            blowholeButton = "p2Blowhole";
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton(blowholeButton))
+        {
+            Splash(gameObject.transform.position, gameObject.transform.rotation);
+        }
+	}
+
+    public void Splash(Vector3 position, Quaternion rotation)
+    {
+        //instantiate the splash effect asset at point of contact
+        GameObject spray = Instantiate(blowholeVisual, position, rotation);
+
+        AnimateSplash(spray);
+    }
+
+    void AnimateSplash(GameObject splash)
+    {
+        splash.transform.position += (1 * Vector3.up);
+        //destroy splash in 3 seconds
+        Destroy(splash, 3);
+    }
+}
