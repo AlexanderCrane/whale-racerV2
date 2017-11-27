531761e023f99eb76e3369a43d2934348e1aa9e1
Alex Crane <InnovateTheWorld@yahoo.com>
2017-11-26 23:43:41 -0600

    Bunch of stuff, lot of frustrations, start of second map

---
+++ Whale Racer V2/Assets/ShipMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMove : MonoBehaviour {
+
+    public static int movespeed = 1;
+    public Vector3 forwardDirection = Vector3.forward;
+    public int i = 0;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (i < 10)
+        {
+            transform.Translate(forwardDirection * movespeed * Time.deltaTime);
+            i++;
+        }
+        if (i == 10)
+        {
+            transform.rotation = Quaternion.AngleAxis(180, Vector3.up);
+            i = 0;
+        }
+
+    }
+}
